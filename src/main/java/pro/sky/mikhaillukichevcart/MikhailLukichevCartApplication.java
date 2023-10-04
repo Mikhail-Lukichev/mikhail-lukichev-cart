cd3a14913619857cfacad4e888bedfadd325a623
Mikhail Lukichev <dextrumm@gmail.com>
2023-10-04 12:05:48 -0600

    Initial commit

---
+++ src/main/java/pro/sky/mikhaillukichevcart/MikhailLukichevCartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.mikhaillukichevcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikhailLukichevCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikhailLukichevCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/mikhaillukichevcart/MikhailLukichevCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.mikhaillukichevcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhailLukichevCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
